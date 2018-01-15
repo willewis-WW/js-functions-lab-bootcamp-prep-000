9cf28fe5d9ac47939a72c698b369f8326320794b
William Lewis <william.lewis@wework.com>
2018-01-15 18:30:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ function happyHolidays() {
   return 'Happy holidays!'
 }
 
+
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
+}
+
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}`
 }
